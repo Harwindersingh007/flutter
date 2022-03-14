58cc44c4cbf3957802b36fc38507839699e63b15
Harwindersingh007 <harwinderoaesolutions@gmail.com>
2022-03-14 23:29:56 +0530

    day 6 completed

---
+++ myfirstapp/lib/widgets/drawer.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: non_constant_identifier_names, prefer_const_declarations
+    final Imagename = "assets/images/harry.JPG";
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          //padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                    decoration: const BoxDecoration(color: Colors.deepPurple),
+                    accountName: const Text("Harwinder singh"),
+                    accountEmail: const Text("Harwinderoaesolutions@gmail.com"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: AssetImage(Imagename),
+                    ))),
+            const ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                )),
+            const ListTile(
+                leading: Icon(
+                  CupertinoIcons.profile_circled,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Profile",
+                  style: TextStyle(color: Colors.white),
+                )),
+            const ListTile(
+                leading: Icon(
+                  CupertinoIcons.mail,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Email me",
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
